ea314ebeaae8908193054f3acdcc89c07d1694eb
Lewis Grint <lgrint1@yahoo.co.uk>
2019-05-31 23:20:26 +0100

    Switch to using express with some esm thrown in for using ES6 constructs within node (wanted to us start splitting things out and importing/exporting functionality).

---
+++ node-atp-example/Utility/base.js
@@ -0,0 +1,9 @@
+export const elementSelectors = {
+    atpTableId: '#u868'
+};
+
+export const httpUtils = {
+    atpLiveRankingSite: 'https://live-tennis.eu/en/atp-live-ranking',
+    proxy: 'https://cors-anywhere.herokuapp.com/',
+    port: 1337
+}
--- node-atp-example/atp-example.js
+++ node-atp-example/atp-example.js
@@ -1,72 +1,34 @@
-// Stock package for creating a simple node server
-const http = require('http');
+// Excellent tutorial for using import/export (ES6 gubbins) here! https://timonweb.com/tutorials/how-to-enable-ecmascript-6-imports-in-nodejs/
+import AtpDataCollectionHelper from "./Models/AtpDataCollectionHelper";
+import { httpUtils } from "./Utility/base";
 
-// Another utility package is used for url inspection/routing capabilities
-const url = require('url');
+// Use 'express' for our stock server (handles routing, etc.)
+const express = require('express');
 
-// Axios (AJAX for returning live ranking HTML 'raw' data)
-const axios = require('axios');
+// Some very simple debug switching, if needed along with our server variable
+const debug = true;
+const app = express();
 
-// Cheerio included to scrape HTML and provide manipulated content
-const cheerio = require('cheerio')
-
-class AtpDataCollectionHelper {
-    constructor(url) {
-        this.url = url;
-        this.proxy = 'https://cors-anywhere.herokuapp.com/';
+app.get('/', async (req, res) => {
+    if (debug) {
+        console.log('Request logged for a resource...');
     }
-
-    // Return ATP live ranking results (from the designated URL - from an HTML page currently)
-    async getResults() {
-        try {
-            const res = await axios.get(`${this.proxy}${this.url}`, { 
-                crossdomain: true, 
-                headers: {
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            });
-
-            this.liveRankingResultsHtml = res.data;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-}
-
-// Server setup - when our server is hit the callback function is invoked. Setup to listen to port 1337 on localhost
-const server = http.createServer(async (req, res) => {
-    console.log('Request logged!');
 
     // Quick helper class to manage the return of results/manipulation of data
-    const dataCollectionHelper = new AtpDataCollectionHelper('https://live-tennis.eu/en/atp-live-ranking');
-    await dataCollectionHelper.getResults();
+    const dataCollectionHelper = new AtpDataCollectionHelper(httpUtils.atpLiveRankingSite);
+    await dataCollectionHelper.getResults();  
 
-    // Confirm that the data type is of type 'string' (dummy)
-    console.log(`Results data type: ${typeof dataCollectionHelper.liveRankingResultsHtml}`);
+    if (debug) {
+        // Confirm that the data type is of type 'string' (dummy)
+        console.log(`Results data type: ${typeof dataCollectionHelper.liveRankingResultsHtml}`);
+    }
 
-    // Use cheeriojs to obtain the 'ranking' HTML table (just going for the first 10 results, for now) - just want to inspect the text, as a test
-    const $ = cheerio.load(dataCollectionHelper.liveRankingResultsHtml);
-    const elements = $('#u868').children('tbody').find('tr');
-
-    // Temp code only - attempt to scrape the HTML using fairly fast/loose index based grabs on td text
-    const playerDetailsTemp = [];
-
-    elements.slice(0, 20).each((i, elem) => {
-        if (i % 2 === 0) {
-            playerDetailsTemp.push({
-                rank: parseInt($(elem).find('td').eq(0).text().trim()),
-                name: $(elem).find('td').eq(3).text(),
-                age: Math.floor(parseFloat($(elem).find('td').eq(4).text())),
-                country: $(elem).find('td').eq(5).text(),
-                points: parseInt($(elem).find('td').eq(6).text()),
-            });
-        }
-    });
+    const playerDetailsTemp = dataCollectionHelper.getPlayerJsonFromHtml();
 
     // Again, return result as JSON, for now (until we do some proper templating/routing, etc.)
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(playerDetailsTemp));
+    res.send(JSON.stringify(playerDetailsTemp));
 });
-server.listen(1337, '127.0.0.1', () => {
-    console.log('Server started. Listening on 127.0.0.1 on port 1337...');
-});
+
+// Kick off our server!
+app.listen(httpUtils.port, '127.0.0.1', () => console.log(`Server started. Listening on 127.0.0.1 on port ${httpUtils.port}...`));
