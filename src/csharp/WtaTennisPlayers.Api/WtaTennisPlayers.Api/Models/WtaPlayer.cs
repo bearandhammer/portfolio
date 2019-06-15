256a075433c34ed091f7ac967b10ab246736f142
Lewis Grint <lgrint1@yahoo.co.uk>
2019-06-15 16:28:47 +0100

    As Tennis is the current 'theme' for the code I'm writing here is the starter for a sample showing some of the functionality that OData can provide (layered on top of a .NET Core API). Starter project only at this point.

---
+++ src/csharp/WtaTennisPlayers.Api/WtaTennisPlayers.Api/Models/WtaPlayer.cs
@@ -0,0 +1,16 @@
+﻿namespace WtaTennisPlayers.Api.Models
+{
+    public class WtaPlayer
+    {
+        public int Rank { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+        public WtaPlayer(int playerRank, string playerName, int playerPoints)
+        {
+            Rank = playerRank;
+            Name = playerName;
+            Points = playerPoints;
+        }
+    }
+}
