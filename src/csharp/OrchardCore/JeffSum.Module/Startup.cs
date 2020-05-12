88c7cb7e9eddce8b45202e3304ddf54f27a15a03
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-12 18:57:13 +0100

    Following the stock example at the moment.
    Including the WeatherForceast controller as a module. Works so far!

---
+++ src/csharp/OrchardCore/JeffSum.Module/Startup.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Routing;
+
+namespace JeffSum.Module
+{
+    public class Startup
+    {
+        public void Configure(IEndpointRouteBuilder endpoints)
+        {
+
+        }
+    }
+}
