3ed08eb967f99991a663310b2868151f13c88c46
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-13 19:22:04 +0100

    DinoIpsum Module added.
    From this point forward I just need to tidy code, further share logic and prep for a full blog post.

--- src/csharp/OrchardCore/DinoIpsum.Services/DinoIpsumService.cs
+++ src/csharp/OrchardCore/DinoIpsum.Services/DinoIpsumService.cs
@@ -1,15 +1,25 @@
 ﻿using DinoIpsum.Services.Interfaces;
-using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DinoIpsum.Services
 {
     public class DinoIpsumService : IDinoIpsumService
     {
-        public Task<List<string>> GetIpsumParagraphs(int paragraphCount)
+        public async Task<List<string>> GetIpsumParagraphs(int paragraphCount)
         {
-            throw new NotImplementedException();
+            List<List<string>> generatedParagraphs = new List<List<string>>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                string response = await client.GetStringAsync("http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=30");
+                generatedParagraphs.AddRange(JsonConvert.DeserializeObject<List<List<string>>>(response));
+            }
+
+            return generatedParagraphs.Select(gp => string.Join(" ", gp)).ToList();
         }
     }
 }
--- src/csharp/OrchardCore/DinoIpsum/Controllers/DinoController.cs
+++ src/csharp/OrchardCore/DinoIpsum/Controllers/DinoController.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using DinoIpsum.Module.ViewModels;
+using DinoIpsum.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace DinoIpsum.Module.Controllers
 {
-    public class DinoController
+    [Route("GiveMeDino")]
+    public class DinoController : Controller
     {
+        public async Task<IActionResult> Index([FromServices] IDinoIpsumService dinoIpsumService) =>
+            View(new DinoIpsumViewModel(await dinoIpsumService.GetIpsumParagraphs(3)));
     }
 }
