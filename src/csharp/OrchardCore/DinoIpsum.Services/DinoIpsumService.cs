e8e7611bc137d1651d873d140d54e0efb6790d21
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-13 21:19:49 +0100

    Abstracted API calls to generic base method.
    Primed for sharing.

--- src/csharp/OrchardCore/DinoIpsum.Services/DinoIpsumService.cs
+++ src/csharp/OrchardCore/DinoIpsum.Services/DinoIpsumService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Modules.Shared.Configuration;
 using Newtonsoft.Json;
+using Services.Shared.Base;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -9,26 +10,19 @@ using System.Threading.Tasks;
 
 namespace DinoIpsum.Services
 {
-    public class DinoIpsumService : IDinoIpsumService
+    public class DinoIpsumService : BaseApiService, IDinoIpsumService
     {
-        private readonly IConfiguration configurationProvider;
-
         public DinoIpsumService(IConfiguration configProvider)
+            : base(configProvider)
         {
-            configurationProvider = configProvider;
+
         }
 
         public async Task<List<string>> GetIpsumParagraphsAsync(int paragraphCount)
         {
-            string apiUrl = string.Format(configurationProvider.GetSection(ConfigurationKeys.ApiConfig).Get<ApiConfiguration>().DinoIpsumApiUrl, paragraphCount);
+            string apiUrl = string.Format(ConfigurationProvider.GetSection(ConfigurationKeys.ApiConfig).Get<ApiConfiguration>().DinoIpsumApiUrl, paragraphCount);
 
-            List<List<string>> generatedParagraphs = new List<List<string>>();
-
-            using (HttpClient client = new HttpClient())
-            {
-                string response = await client.GetStringAsync(apiUrl);
-                generatedParagraphs.AddRange(JsonConvert.DeserializeObject<List<List<string>>>(response));
-            }
+            List<List<string>> generatedParagraphs = await GetApiResultAsync<List<List<string>>>(apiUrl);
 
             return generatedParagraphs.Select(gp => string.Join(" ", gp)).ToList();
         }
--- src/csharp/OrchardCore/HipsterIpsum.Services/HipsterIpsumService.cs
+++ src/csharp/OrchardCore/HipsterIpsum.Services/HipsterIpsumService.cs
@@ -1,35 +1,24 @@
 ﻿using HipsterIpsum.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Modules.Shared.Configuration;
-using Newtonsoft.Json;
+using Services.Shared.Base;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HipsterIpsum.Services
 {
-    public class HipsterIpsumService : IHipsterIpsumService
+    public class HipsterIpsumService : BaseApiService, IHipsterIpsumService
     {
-        private readonly IConfiguration configurationProvider;
-
         public HipsterIpsumService(IConfiguration configProvider)
+            : base(configProvider)
         {
-            configurationProvider = configProvider;
         }
 
         public async Task<List<string>> GetIpsumParagraphsAsync(int paragraphCount)
         {
-            string apiUrl = string.Format(configurationProvider.GetSection(ConfigurationKeys.ApiConfig).Get<ApiConfiguration>().HipsterIpsumApiUrl, paragraphCount);
+            string apiUrl = string.Format(ConfigurationProvider.GetSection(ConfigurationKeys.ApiConfig).Get<ApiConfiguration>().HipsterIpsumApiUrl, paragraphCount);
 
-            List<string> generatedParagraphs = new List<string>();
-
-            using (HttpClient client = new HttpClient())
-            {
-                string response = await client.GetStringAsync(apiUrl);
-                generatedParagraphs.AddRange(JsonConvert.DeserializeObject<List<string>>(response));
-            }
-
-            return generatedParagraphs;
+            return await GetApiResultAsync<List<string>>(apiUrl);
         }
     }
 }
