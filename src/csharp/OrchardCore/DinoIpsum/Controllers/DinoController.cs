d6149ab7422365a614f7fbcc1c65a123c8e04364
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-14 21:22:58 +0100

    HipsterIpsum.Module - Tidy of code/comments added.

--- src/csharp/OrchardCore/DinoIpsum/Controllers/DinoController.cs
+++ src/csharp/OrchardCore/DinoIpsum/Controllers/DinoController.cs
@@ -16,7 +16,7 @@ namespace DinoIpsum.Module.Controllers
         /// dino-related Lorem Ipsum content to the appropriate View.
         /// </summary>
         /// <param name="dinoIpsumService">A service calling the relevant API to generate Lorem Ipsum content.</param>
-        /// <returns>dino-related Lorem Ipsum content to the designated View (fixed to 3 paragraphs, in this instance).</returns>
+        /// <returns>Dino-related Lorem Ipsum content to the designated View (fixed to 3 paragraphs, in this instance).</returns>
         public async Task<IActionResult> Index([FromServices] IDinoIpsumService dinoIpsumService) =>
             View(new DinoIpsumViewModel(await dinoIpsumService.GetIpsumParagraphsAsync(3)));
     }
