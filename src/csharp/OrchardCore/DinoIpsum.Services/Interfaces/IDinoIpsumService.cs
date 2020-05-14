2650f387b683f67fd9bd553b4fe2016fb4fb5b13
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-14 20:20:21 +0100

    DinoIpsum.Services - Tidy of code/comments added.

--- src/csharp/OrchardCore/DinoIpsum.Services/Interfaces/IDinoIpsumService.cs
+++ src/csharp/OrchardCore/DinoIpsum.Services/Interfaces/IDinoIpsumService.cs
@@ -2,6 +2,9 @@
 
 namespace DinoIpsum.Services.Interfaces
 {
+    /// <summary>
+    /// Interface for obtaining dino-related Lorem Ipsum content.
+    /// </summary>
     public interface IDinoIpsumService : IIpsumService
     {
     }
--- src/csharp/OrchardCore/DinoIpsum/Controllers/DinoController.cs
+++ src/csharp/OrchardCore/DinoIpsum/Controllers/DinoController.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 namespace DinoIpsum.Module.Controllers
 {
     /// <summary>
-    /// A controller to feed any Dino-related Lorem Ipsum Views.
+    /// A controller to feed any dino-related Lorem Ipsum Views.
     /// </summary>
     [Route("GiveMeDino")]
     public class DinoController : Controller
     {
         /// <summary>
         /// Returns an <see cref="IActionResult"/> for rendering
-        /// Dino-related Lorem Ipsum content to the appropriate View.
+        /// dino-related Lorem Ipsum content to the appropriate View.
         /// </summary>
         /// <param name="dinoIpsumService">A service calling the relevant API to generate Lorem Ipsum content.</param>
-        /// <returns>Dino-related Lorem Ipsum content to the designated View (fixed to 3 paragraphs, in this instance).</returns>
+        /// <returns>dino-related Lorem Ipsum content to the designated View (fixed to 3 paragraphs, in this instance).</returns>
         public async Task<IActionResult> Index([FromServices] IDinoIpsumService dinoIpsumService) =>
             View(new DinoIpsumViewModel(await dinoIpsumService.GetIpsumParagraphsAsync(3)));
     }
