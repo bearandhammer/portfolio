2f2ca668bd69057578ca38cf4921152cc3bc6b21
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-14 21:35:14 +0100

    Ipsum.Generator.Web - Tidy of code/comments added.

--- src/csharp/OrchardCore/Ipsum.Generator.Web/Startup.cs
+++ src/csharp/OrchardCore/Ipsum.Generator.Web/Startup.cs
@@ -6,22 +6,42 @@ using Microsoft.Extensions.Hosting;
 
 namespace Ipsum.Generator.Web
 {
+    /// <summary>
+    /// Stock configuration, out of the box, for a Startup
+    /// file for a general web application.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// Represents the out of the box configuration.
+        /// </summary>
+        /// <param name="configuration">An <see cref="IConfiguration"/> type for accessing application settings.</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Gets access to applications settings, as needed.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">An <see cref="IServiceCollection"/> type ready to gather up registered dependencies.</param>
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add the MVC 'flavour' of Orchard Core
             services.AddOrchardCore().AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">Allows alterations to the application's request pipeline.</param>
+        /// <param name="env">Allows access to the web hosting environment the application is running on.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,9 +57,9 @@ namespace Ipsum.Generator.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
 
+            // Configure the application to be a modular, Orchard application
             app.UseOrchardCore();
         }
     }
