0b315e018912bde44779cb725c9066b71c93dc7f
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-14 22:09:11 +0100

    JeffSum.Services - Tidy of code/comments added.

--- src/csharp/OrchardCore/Jeff.Services/Services/JeffIpsumService.cs
+++ src/csharp/OrchardCore/Jeff.Services/Services/JeffIpsumService.cs
@@ -12,7 +12,12 @@ namespace JeffSum.Services.Services
     /// </summary>
     public class JeffIpsumService : IJeffIpsumService
     {
-        /// <inheritdoc/>
+        /// <summary>
+        /// Gets the desired amount of paragraphs of Jeff-related
+        /// Lorem Ipsum content from 'JeffSum' library.
+        /// </summary>
+        /// <param name="paragraphCount">The amount of paragraphs of content to generate.</param>
+        /// <returns>A <see cref="List{string}"/> holding the generated Lorem Ipsum content.</returns>
         public async Task<List<string>> GetIpsumParagraphsAsync(int paragraphCount) => await Task.Run(() => Goldblum.ReceiveTheJeff(paragraphCount).ToList());
     }
 }
