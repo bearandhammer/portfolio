d83dd5ff20617658d6b574b7dcb8514893e71563
Lewis Grint <lgrint1@yahoo.co.uk>
2023-03-05 11:22:11 +0000

    Internal ref code coverage added.

--- src/csharp/MaintainabilityIndex.Sample.Testing/MaintainabilityIndex.Sample.Testing/FoodClassifierTests.cs
+++ src/csharp/MaintainabilityIndex.Sample.Testing/MaintainabilityIndex.Sample.Testing/FoodClassifierTests.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace MaintainabilityIndex.Sample.Testing
 {
     public class FoodClassifierTests
@@ -9,14 +11,49 @@ namespace MaintainabilityIndex.Sample.Testing
             foodClassifier = new FoodClassifier();
         }
 
-        public void GetFoodTypeSampleOne_Data_Test()
+        public static IEnumerable<object[]> GetFoodTypeSampleData()
         {
-
+            foreach (var foodWithInternalRefItem in GetFoodsWithInternalRefTestData())
+            {
+                yield return foodWithInternalRefItem;
+            }
         }
 
-        public void GetFoodTypeSampleTwo_Data_Test()
-        {
+        [Theory]
+        [MemberData(nameof(GetFoodTypeSampleData))]
+        public void GetFoodTypeSampleOne_Data_Test(Food foodUnderTest, FoodType expectedFoodType) =>
+            foodClassifier.GetFoodTypeSampleOne(foodUnderTest)
+                .Should()
+                .Be(expectedFoodType);
 
+        [Theory]
+        [MemberData(nameof(GetFoodTypeSampleData))]
+        public void GetFoodTypeSampleTwo_Data_Test(Food foodUnderTest, FoodType expectedFoodType) =>
+            foodClassifier.GetFoodTypeSampleOne(foodUnderTest)
+                .Should()
+                .Be(expectedFoodType);
+
+        private static IEnumerable<object[]> GetFoodsWithInternalRefTestData()
+        {
+            yield return new object[] { new Food { InternalRef = "b" }, FoodType.Biscuits };
+            yield return new object[] { new Food { InternalRef = "bc" }, FoodType.Biscuits };
+            yield return new object[] { new Food { InternalRef = "d" }, FoodType.Doughnuts };
+            yield return new object[] { new Food { InternalRef = "dn" }, FoodType.Doughnuts };
+            yield return new object[] { new Food { InternalRef = "s" }, FoodType.Sandwiches };
+            yield return new object[] { new Food { InternalRef = "sw" }, FoodType.Sandwiches };
+            yield return new object[] { new Food { InternalRef = "v" }, FoodType.Vegetable };
+            yield return new object[] { new Food { InternalRef = "vg" }, FoodType.Vegetable };
+            yield return new object[] { new Food { InternalRef = "f" }, FoodType.Fruit };
+            yield return new object[] { new Food { InternalRef = "fr" }, FoodType.Fruit };
+            yield return new object[] { new Food { InternalRef = "c" }, FoodType.Cake };
+            yield return new object[] { new Food { InternalRef = "ck" }, FoodType.Cake };
+            yield return new object[] { new Food { InternalRef = "m" }, FoodType.Milkshake };
+            yield return new object[] { new Food { InternalRef = "ms" }, FoodType.Milkshake };
+            yield return new object[] { new Food { InternalRef = "g" }, FoodType.Grain };
+            yield return new object[] { new Food { InternalRef = "gr" }, FoodType.Grain };
+            yield return new object[] { new Food { InternalRef = "r" }, FoodType.RobocopBabyFood };
+            yield return new object[] { new Food { InternalRef = "rb" }, FoodType.RobocopBabyFood };
+            yield return new object[] { new Food { InternalRef = "rbcbf" }, FoodType.RobocopBabyFood };
         }
     }
 }
