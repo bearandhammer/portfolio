2ee73de0c4ec35f0310a2d2e993b4f3524110068
Lewis Grint <lgrint1@yahoo.co.uk>
2023-03-05 11:11:33 +0000

    Stubbed out unit test file.

--- src/csharp/MaintainabilityIndex.Sample.Testing/MaintainabilityIndex.Sample.Testing/FoodClassifierTests.cs
+++ src/csharp/MaintainabilityIndex.Sample.Testing/MaintainabilityIndex.Sample.Testing/FoodClassifierTests.cs
@@ -2,6 +2,21 @@ namespace MaintainabilityIndex.Sample.Testing
 {
     public class FoodClassifierTests
     {
+        private readonly FoodClassifier foodClassifier;
 
+        public FoodClassifierTests()
+        {
+            foodClassifier = new FoodClassifier();
+        }
+
+        public void GetFoodTypeSampleOne_Data_Test()
+        {
+
+        }
+
+        public void GetFoodTypeSampleTwo_Data_Test()
+        {
+
+        }
     }
 }
