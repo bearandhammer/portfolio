0583a5a42e7e4a0a9ca672fa1b796eb1510669a5
Lewis Grint <lgrint1@yahoo.co.uk>
2026-04-01 23:16:48 +0100

    Implement stub of an IExceptionHandler.
    Main element of the sample.

---
+++ src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace Error.Handler.Sample.Api.Handlers
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            // TODO: Implement and register
+            throw new NotImplementedException();
+        }
+    }
+}
