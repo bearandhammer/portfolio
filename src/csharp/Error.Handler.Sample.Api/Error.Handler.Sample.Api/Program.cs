b268fc6617893600c9e787fa68c386ae80a477c0
Lewis Grint <lgrint1@yahoo.co.uk>
2026-04-02 16:36:28 +0100

    Add GlobalExceptionHandler and ProblemDetails.
    app.UseExceptionHandler - I think this is required (to check full usage across environment - dev/prod).

--- src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Program.cs
+++ src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Program.cs
@@ -1,3 +1,4 @@
+using Error.Handler.Sample.Api.Handlers;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -6,6 +7,17 @@ Log.Logger = new LoggerConfiguration()
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
+
+// Add global exception handling (general ProblemDetails and no leaking of stack info!)
+builder.Services.AddProblemDetails(configure =>
+{
+    configure.CustomizeProblemDetails = context =>
+    {
+        // TODO: Consider resource string for request ID
+        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+    };
+});
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 // Add services to the container.
 
@@ -21,10 +33,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseExceptionHandler();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
