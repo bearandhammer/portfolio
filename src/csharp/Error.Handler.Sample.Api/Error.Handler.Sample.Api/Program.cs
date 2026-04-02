a2d8e4468231181569b991f91059c93347949a50
Lewis Grint <lgrint1@yahoo.co.uk>
2026-04-02 16:32:31 +0100

    Include basic Serilog setup (console only).

--- src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Program.cs
+++ src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Program.cs
@@ -1,4 +1,11 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
