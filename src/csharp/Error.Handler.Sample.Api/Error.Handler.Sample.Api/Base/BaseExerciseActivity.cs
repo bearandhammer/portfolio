aa73476283cc49e4d9daee6b072faeb7292475a1
Lewis Grint <lgrint1@yahoo.co.uk>
2026-04-06 14:27:31 +0100

    Stubbed out request/response with a shared base.
    A little less useful, atm, with records.

--- src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Base/BaseExerciseActivity.cs
+++ src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Base/BaseExerciseActivity.cs
@@ -1,4 +1,10 @@
 ﻿namespace Error.Handler.Sample.Api.Base
 {
-    public record BaseExerciseActivity(string Description, int AverageHeartRate, int MaximumHeartRate);
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseExerciseActivity"/> class.
+    /// </summary>
+    /// <param name="Description">The description of the exercise activity (directly from the user).</param>
+    /// <param name="AverageHeartRate">The average heart rate recorded during the activity.</param>
+    /// <param name="MaximumHeartRate">The maximum heart rate recorded during the activity.</param>
+    public abstract record BaseExerciseActivity(string Description, int AverageHeartRate, int MaximumHeartRate);
 }
