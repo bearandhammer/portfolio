74163879ab2e1d63761f29b1f13fa63e6a96bba1
Lewis Grint <lgrint1@yahoo.co.uk>
2026-04-06 14:16:14 +0100

    Add stock exercise request and response types.

---
+++ src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Requests/AddExerciseActivityRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Error.Handler.Sample.Api.Requests
+{
+    public record AddExerciseActivityRequest(string Description, int AverageHeartRate, int MaximumHeartRate);
+}
---
+++ src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api/Responses/ExerciseActivityResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace Error.Handler.Sample.Api.Responses
+{
+    public record ExerciseActivityResponse(Guid UniqueId, string Description, int AverageHeartRate, int MaximumHeartRate);
+}
