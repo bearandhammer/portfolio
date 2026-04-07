9fd75de756ab9587e2bd86365d641d494b3deb75
Lewis Grint <lgrint1@yahoo.co.uk>
2026-04-07 19:33:58 +0100

    Update entity (store model) and repo.
    More XML comment generation from AI, for kicks.

--- src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api.Repository/Implementation/ExerciseActivityRepo.cs
+++ src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api.Repository/Implementation/ExerciseActivityRepo.cs
@@ -5,12 +5,21 @@ using LiteDB;
 
 namespace Error.Handler.Sample.Api.Repository.Implementation
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ExerciseActivityRepo"/> class.
+    /// Represents an implementation of the <see cref="IExerciseActivityRepo"/> interface that provides persistence operations for exercise activities in the backing store.
+    /// </summary>
+    /// <param name="context">The <see cref="LiteDbContext"/> instance in scope to use for persistence operations.</param>
+    /// <seealso cref="IExerciseActivityRepo"/>
     public sealed class ExerciseActivityRepo(LiteDbContext context) : IExerciseActivityRepo
     {
+        /// <summary>
+        /// Represents the <see cref="LiteDbContext"/> instance in scope to use for persistence operations.
+        /// </summary>
         private readonly LiteDbContext _context = context;
 
         /// <inheritdoc />
-        public ExerciseActivityEntity AddExerciseActivity(ExerciseActivityEntity exerciseActivityToAdd)
+        public ExerciseActivityEntity AddExerciseActivityToStore(ExerciseActivityEntity exerciseActivityToAdd)
         {
             Guid insertedId =
                 _context
@@ -23,28 +32,28 @@ namespace Error.Handler.Sample.Api.Repository.Implementation
         }
 
         /// <inheritdoc />
-        public bool DeleteExerciseActivity(Guid id) =>
+        public bool DeleteExerciseActivityFromStore(Guid id) =>
             _context
                 .ActivityDatabase
                 .GetCollection<ExerciseActivityEntity>()
                 .Delete(new BsonValue(id));
 
         /// <inheritdoc />
-        public List<ExerciseActivityEntity> GetExerciseActivities() =>
+        public List<ExerciseActivityEntity> GetExerciseActivitiesFromStore() =>
             [.. _context
                 .ActivityDatabase
                 .GetCollection<ExerciseActivityEntity>(_context.CollectionName)
                 .FindAll()];
 
         /// <inheritdoc />
-        public ExerciseActivityEntity GetExerciseActivityById(Guid id) =>
+        public ExerciseActivityEntity GetExerciseActivityFromStoreById(Guid id) =>
             _context
                 .ActivityDatabase
                 .GetCollection<ExerciseActivityEntity>(_context.CollectionName)
                 .FindById(id);
 
         /// <inheritdoc />
-        public (ExerciseActivityEntity exerciseActivity, bool successfullyUpdated) UpdateExerciseActivity(ExerciseActivityEntity exerciseActivityToUpdate)
+        public (ExerciseActivityEntity exerciseActivity, bool successfullyUpdated) UpdateExerciseActivityInStore(ExerciseActivityEntity exerciseActivityToUpdate)
         {
             bool successfullyUpdated = _context
                     .ActivityDatabase
--- src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api.Repository/Interface/IExerciseActivityRepo.cs
+++ src/csharp/Error.Handler.Sample.Api/Error.Handler.Sample.Api.Repository/Interface/IExerciseActivityRepo.cs
@@ -12,27 +12,27 @@ namespace Error.Handler.Sample.Api.Repository.Interface
         /// </summary>
         /// <param name="exerciseActivityToAdd">The exercise activity to persist. The <see cref="ExerciseActivityEntity.Id"/> may be updated to match the store-generated key.</param>
         /// <returns>The same <paramref name="exerciseActivityToAdd"/> instance with <see cref="ExerciseActivityEntity.Id"/> set to the inserted identifier.</returns>
-        ExerciseActivityEntity AddExerciseActivity(ExerciseActivityEntity exerciseActivityToAdd);
+        ExerciseActivityEntity AddExerciseActivityToStore(ExerciseActivityEntity exerciseActivityToAdd);
 
         /// <summary>
         /// Deletes the exercise activity with the specified identifier, if it exists.
         /// </summary>
         /// <param name="id">The unique identifier of the exercise activity to remove.</param>
         /// <returns><see langword="true"/> if a document was deleted; otherwise, <see langword="false"/>.</returns>
-        bool DeleteExerciseActivity(Guid id);
+        bool DeleteExerciseActivityFromStore(Guid id);
 
         /// <summary>
         /// Retrieves all exercise activities from the backing store.
         /// </summary>
         /// <returns>A list of all persisted exercise activities. The list may be empty if none exist.</returns>
-        List<ExerciseActivityEntity> GetExerciseActivities();
+        List<ExerciseActivityEntity> GetExerciseActivitiesFromStore();
 
         /// <summary>
         /// Retrieves the exercise activity with the specified identifier.
         /// </summary>
         /// <param name="id">The unique identifier of the exercise activity to load.</param>
         /// <returns>The matching <see cref="ExerciseActivityEntity"/>, or <see langword="null"/> if no document exists for <paramref name="id"/>.</returns>
-        ExerciseActivityEntity GetExerciseActivityById(Guid id);
+        ExerciseActivityEntity GetExerciseActivityFromStoreById(Guid id);
 
         /// <summary>
         /// Updates an existing exercise activity in the backing store.
@@ -42,7 +42,7 @@ namespace Error.Handler.Sample.Api.Repository.Interface
         /// A tuple where <c>exerciseActivity</c> is the same instance as <paramref name="exerciseActivityToUpdate"/>,
         /// and <c>successfullyUpdated</c> indicates whether the backing store reported a successful update.
         /// </returns>
-        (ExerciseActivityEntity exerciseActivity, bool successfullyUpdated) UpdateExerciseActivity(
+        (ExerciseActivityEntity exerciseActivity, bool successfullyUpdated) UpdateExerciseActivityInStore(
             ExerciseActivityEntity exerciseActivityToUpdate);
     }
 }
